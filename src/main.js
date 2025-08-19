c1c72ea17053813f27251d6c1947bb09c54bb0eb
dishwasher-detergent <kenny_bass@outlook.com>
2025-08-19 08:15:49 -0500

    test

--- src/main.js
+++ src/main.js
@@ -1,35 +1,30 @@
-import { Client, Users } from 'node-appwrite';
+import { Client, Databases, ID } from "node-appwrite";
 
-// This Appwrite function will be executed every time your function is triggered
 export default async ({ req, res, log, error }) => {
-  // You can use the Appwrite SDK to interact with other services
-  // For this example, we're using the Users service
   const client = new Client()
     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
-    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(req.headers['x-appwrite-key'] ?? '');
-  const users = new Users(client);
+    .setProject("68a47889001b14e0eb8c")
+    .setKey(req.headers["x-appwrite-key"]);
 
+  const databases = new Databases(client);
+
+  const body = req.bodyJson;
+    
   try {
-    const response = await users.list();
-    // Log messages and errors to the Appwrite Console
-    // These logs won't be seen by your end users
-    log(`Total users: ${response.total}`);
-  } catch(err) {
-    error("Could not list users: " + err.message);
+    const res = await databases.createDocument(
+      process.env.DATABASE_ID,
+      process.env.COLLECTION_ID,
+      ID.unique(),
+      body
+    );
+
+    console.log(`Response: ${JSON.stringify(res)}`);
+
+  } catch (e) {
+    console.log(e);
+
+    return res.text("error");
   }
 
-  // The req object contains the request data
-  if (req.path === "/ping") {
-    // Use res object to respond with text(), json(), or binary()
-    // Don't forget to return a response!
-    return res.text("Pong");
-  }
-
-  return res.json({
-    motto: "Build like a team of hundreds_",
-    learn: "https://appwrite.io/docs",
-    connect: "https://appwrite.io/discord",
-    getInspired: "https://builtwith.appwrite.io",
-  });
+  return res.empty();
 };
