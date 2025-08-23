e51de676ed09d30de46c63a3836a7ade9f49e362
dishwasher-detergent <kenny_bass@outlook.com>
2025-08-23 18:07:11 -0500

    test

--- src/main.js
+++ src/main.js
@@ -1,29 +1,5 @@
 import { Client, Databases, ID } from "node-appwrite";
 
 export default async ({ req, res, log, error }) => {
-  const client = new Client()
-    .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
-    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(req.headers["x-appwrite-key"]);
-
-  const databases = new Databases(client);
-  const body = req.bodyJson;
-    
-  try {
-    const res = await databases.createDocument(
-      process.env.DATABASE_ID,
-      process.env.COLLECTION_ID,
-      ID.unique(),
-      body
-    );
-
-    console.log(`Response: ${JSON.stringify(res)}`);
-
-  } catch (e) {
-    console.log(e);
-
-    return res.text("error");
-  }
-
-  return res.empty();
+  return res.json({"test": "test"}, 500)
 };
